aa47f4241d0471004a5d0b490418b03c42116940
hgg <hgg>
2016-05-25 23:15:42 +0800

    merge_sort unfinish

--- list/sort/sort.h
+++ list/sort/sort.h
@@ -1,5 +1,6 @@
 #include "../splice/splice.h"
 #include "../../functional/functional.h"
+#include "../advance/advance.cc"
 namespace hgg{
     template <typename Type>
     void swap(Type &left, Type &right) {
@@ -8,13 +9,40 @@ namespace hgg{
         right = tmp;
     }
     template <typename _BI, typename _F>
-    void buble_sort(_BI first, _BI last, _F fun) {
+    void choose_sort(_BI first, _BI last, _F fun) {
         for (; first != last; ++first) {
             _BI min_it = first;
             for (_BI start=first; start!=last; ++start) {
                 if (fun(*start, *min_it)) min_it = start;
             }
             swap(*min_it, *first);
+        }
+    }
+    template <typename _BI, typename _F>
+    void buble_sort(_BI first, _BI last, _F fun) {
+        _BI tail = --last;
+        for (_BI start=first; start!=last; ++start, --tail) {    //control circle time
+            for (_BI head=first; head!=tail;) {
+                _BI prev = head++;
+                if (fun(*prev, *head)) swap(*prev, *head);
+            }
+        }
+    }
+    template <typename _BI>
+    typename _BI::difference_type distance(_BI first, _BI last) {
+        _BI::difference_type i = 0;
+        while (first != last) {
+            ++first;
+            ++i;
+        }
+        return i;
+    }
+    template <typename _BI, typename _F>
+    void merge_sort(_BI first, _BI last, _F fun) {
+        _BI::difference_type size = distance(first, last);
+        int s = 1;
+        while (s<size) {
+            _BI next = first;
         }
     }
 }
